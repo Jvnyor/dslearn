69eb4c56f1ed9b1672316725e27d83ed2f5d579a
Jvnyor <josias100junior@gmail.com>
2022-09-19 11:48:54 -0300

    Alterations: Notification, Deliver, Topic and Reply Fix: missing associations, getters Fix: Remove user roles on constructor

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -1,30 +1,30 @@
 package com.devsuperior.dslearnbds.entities;
 
-import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Objects;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
 @Entity
 @Table(name = "tb_enrollment")
-public class Enrollment implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class Enrollment {
 
 	@EmbeddedId
 	private EnrollmentPK id = new EnrollmentPK();
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant enrollMoment;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant refundMoment;
 	private boolean available;
@@ -33,11 +33,15 @@ public class Enrollment implements Serializable {
 	@ManyToMany(mappedBy = "enrollmentsDone")
 	private Set<Lesson> lessonsDone = new HashSet<>();
 	
+	@OneToMany(mappedBy = "enrollment")
+	private List<Deliver> deliveries = new ArrayList<>();
+	
 	public Enrollment() {
 	}
 
 	public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available,
 			boolean onlyUpdate) {
+		super();
 		id.setUser(user);
 		id.setOffer(offer);
 		this.enrollMoment = enrollMoment;
@@ -45,7 +49,7 @@ public class Enrollment implements Serializable {
 		this.available = available;
 		this.onlyUpdate = onlyUpdate;
 	}
-	
+
 	public User getStudent() {
 		return id.getUser();
 	}
@@ -53,11 +57,11 @@ public class Enrollment implements Serializable {
 	public void setStudent(User user) {
 		id.setUser(user);
 	}
-	
+
 	public Offer getOffer() {
 		return id.getOffer();
 	}
-
+	
 	public void setOffer(Offer offer) {
 		id.setOffer(offer);
 	}
@@ -94,13 +98,16 @@ public class Enrollment implements Serializable {
 		this.onlyUpdate = onlyUpdate;
 	}
 
-	public Set<Lesson> getLessonsDone() {
-		return lessonsDone;
+	public List<Deliver> getDeliveries() {
+		return deliveries;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -112,7 +119,11 @@ public class Enrollment implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Enrollment other = (Enrollment) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -14,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -27,25 +30,29 @@ public abstract class Lesson implements Serializable {
 	private Long id;
 	private String title;
 	private Integer position;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "section_id")
 	private Section section;
-
+	
+	@OneToMany(mappedBy = "lesson")
+	private List<Deliver> deliveries = new ArrayList<>();
+	
 	@ManyToMany
 	@JoinTable(name = "tb_lessons_done",
-	joinColumns = @JoinColumn(name = "lesson_id"),
-	inverseJoinColumns = {
-			@JoinColumn(name = "user_id"),
-			@JoinColumn(name = "offer_id")
-	}
+		joinColumns = @JoinColumn(name = "lesson_id"),
+		inverseJoinColumns = {
+				@JoinColumn(name = "user_id"),
+				@JoinColumn(name = "offer_id")
+		}
 	)
 	private Set<Enrollment> enrollmentsDone = new HashSet<>();
-
+	
 	public Lesson() {
 	}
 
 	public Lesson(Long id, String title, Integer position, Section section) {
+		super();
 		this.id = id;
 		this.title = title;
 		this.position = position;
@@ -88,4 +95,32 @@ public abstract class Lesson implements Serializable {
 		return enrollmentsDone;
 	}
 
+	public List<Deliver> getDeliveries() {
+		return deliveries;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lesson other = (Lesson) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
